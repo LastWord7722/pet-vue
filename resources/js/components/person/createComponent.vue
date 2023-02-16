1e9329cbc927706c141b560fc72d3131add2f1b9
LastWord7722 <LastWordMark@gmail.com>
2023-02-16 15:00:19 +0200

    added store api and component

---
+++ resources/js/components/person/createComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <form >
+    <div class="mb-3">
+      <label class="form-label">name</label>
+      <input type="text" class="form-control" id="name">
+    </div>
+
+    <div class="mb-3">
+      <label class="form-label">age</label>
+      <input type="number" class="form-control" id="age">
+    </div>
+
+    <button type="submit" class="btn btn-primary">Добавить</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "create"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/person/index.component.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <create-component></create-component>
+  </div>
+</template>
+
+<script>
+import createComponent from "./createComponent.vue";
+export default {
+  name: "person",
+
+  data(){
+    return{
+      person : null
+    }
+  },
+
+/*  mounted () {
+    this.getPersons()
+  },
+
+  methods : {
+    getPersons() {
+      axios.get('/public/main')
+          .then(res => {
+            this.person = res.data
+          })
+    },
+  },*/
+
+  components: {
+    createComponent
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
