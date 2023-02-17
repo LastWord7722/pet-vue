fb59a84e570fb588ce970811956589d309dafe37
LastWord7722 <LastWordMark@gmail.com>
2023-02-17 11:23:04 +0200

    update component, for editing

--- resources/js/components/person/index.component.vue
+++ resources/js/components/person/index.component.vue
@@ -8,11 +8,20 @@
     </tr>
     </thead>
     <tbody class="table-group-divider">
-    <tr v-for="persons in persons">
-      <th scope="row">{{ persons.id}}</th>
-      <td>{{ persons.name}}</td>
-      <td>{{ persons.age}}</td>
-    </tr>
+      <template v-for="persons in persons">
+        <tr >
+          <th scope="row">{{ persons.id}}</th>
+          <td>{{ persons.name}}</td>
+          <td>{{ persons.age}}</td>
+          <td><a href="" @click.prevent="changeEditPersonId(persons.id)" class="btn btn-success"> Edit</a></td>
+        </tr>
+        <tr :class="isEdit(persons.id) ? '' :'d-none' ">
+          <th scope="row"> ОБНОВЛЕНИЕ !!!</th>
+          <td><input type="text" class="form-control"></td>
+          <td><input type="number" class="form-control"></td>
+          <td><a href="" @click.prevent="changeEditPersonId(null)" class="btn btn-success"> Update </a></td>
+        </tr>
+      </template>
     </tbody>
   </table>
 </template>
@@ -24,7 +33,8 @@ export default {
 
   data(){
     return{
-      persons : []
+      persons : [],
+      editPersonId : null,
     }
   },
 
@@ -39,6 +49,15 @@ export default {
             this.persons = res.data
           })
     },
+
+    changeEditPersonId(id){
+      this.editPersonId = id
+    },
+
+    isEdit(id){
+      return this.editPersonId === id
+    }
+
   },
 
   components: {
